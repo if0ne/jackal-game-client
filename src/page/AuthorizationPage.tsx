f4f31c09836922d8c00ca86f4a205e7614b6fdec
if0ne <pavel.agafonov.al@gmail.com>
2022-07-11 16:45:02 +0300

    removed google auth

--- src/page/AuthorizationPage.tsx
+++ src/page/AuthorizationPage.tsx
@@ -11,19 +11,23 @@ import yandexLogo from "../assets/yandex_logo.svg";
 //@ts-ignore
 import vkLogo from "../assets/vk_logo.svg";
 
+//@ts-ignore
+import googleLogo from "../assets/google_logo.png";
+
 function AuthorizationPage() {
     const vkCodeUrl = "https://oauth.vk.com/authorize?client_id=8212997&display=popup&redirect_uri=http://localhost:3000/getToken&scope=offline&response_type=code&v=5.131";
 
-    /*const vkAccessTokenUrl = (code: string) => {
-        return `https://oauth.vk.com/access_token?client_id=8212997&client_secret=6D93pYVDAGf4F8mABXkp&redirect_uri=http://localhost:3000/getToken&code=${code}&v=5.131`;
-    }*/
-
     const yandexAuthorizeUrl = "https://oauth.yandex.ru/authorize?response_type=token&client_id=e431653fdda94550bf9d9941bca409b3&redirect_uri=http://localhost:3000/getToken"
+
+    const googleCodeUrl = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=582286245104-phr8rri2nd5bmqdmufsdsaokcq8qos7b.apps.googleusercontent.com&scope=openid%20email&redirect_uri=http://localhost:3000/getToken";
 
     const [token, setToken] = useState(null);
 
     //TODO: Разбить на компонеты
     const loginViaYandex = () => {
+        //@ts-ignore
+        window.SetTokenCallback = undefined;
+
         //@ts-ignore
         window.SetTokenCallback = (url: string) => {
             let query = queryString.parse(url);
@@ -34,11 +38,12 @@ function AuthorizationPage() {
         }
 
         window.open(yandexAuthorizeUrl, 'auth', "menubar=no, location=no, resizable=no, scrollbars=no, status=no, width=800, height=600, top=100, left=100");
-        //@ts-ignore
-        window.SetTokenCallback = undefined;
     }
 
     const loginViaVk = () => {
+        //@ts-ignore
+        window.SetTokenCallback = undefined;
+
         //@ts-ignore
         window.SetTokenCallback = (url: string) => {
             let query = queryString.parse(url);
@@ -49,26 +54,7 @@ function AuthorizationPage() {
         }
 
         window.open(vkCodeUrl, 'auth', "menubar=no, location=no, resizable=no, scrollbars=no, status=no, width=800, height=600, top=100, left=100");
-        //@ts-ignore
-        window.SetTokenCallback = undefined;
     }
-
-    useEffect(() => {
-        //@ts-ignore
-        google.accounts.id.initialize({
-            client_id: "582286245104-phr8rri2nd5bmqdmufsdsaokcq8qos7b.apps.googleusercontent.com",
-            callback: (response: any) => {
-                let info = jwtDecode(response.credential);
-                console.log(info);
-            }
-        });
-
-        //@ts-ignore
-        google.accounts.id.renderButton(
-            document.getElementById("signInGoogleDiv"),
-            { theme: "outline", size: "large" }
-        );
-    }, [])
 
     const logout = () => {
         setToken(null);
@@ -80,9 +66,6 @@ function AuthorizationPage() {
                 <Row className="bg-white shadow-sm">
                     <Col className="border rounded p-4 btn-container">
                         <h1 className="login-title">Войти</h1>
-
-                        <div id="signInGoogleDiv" className="btn-google py-1">
-                        </div>
 
                         <div className="py-1">
                             <button className="btn btn-logo btn-yandex" onClick={loginViaYandex}>
